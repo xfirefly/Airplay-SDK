b83a3d55535e78c8882e9c628d20784bfe0bb8e5
ZY <zsxzy@qq.com>
2020-02-21 23:21:32 +0800

    update

--- pages/themes/hugo-tranquilpeak-theme/tasks/config/exec.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-  grunt.config.set('exec', {
-    eslint: {
-      cmd: './node_modules/eslint/bin/eslint.js .'
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-exec');
-};
